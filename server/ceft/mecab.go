2a1720cca10e57746824d0f2d1b396a5959054a2
Kaoru Tisen <cordx56@cordx.net>
2018-07-30 22:22:40 +0900

    modify and cleanup code

--- server/ceft/mecab.go
+++ server/ceft/mecab.go
@@ -6,20 +6,11 @@ import (
 	"github.com/shogo82148/go-mecab"
 )
 
-// Morp Morpheme struct (s: surface, f: feature)
-type Morp struct {
-	Surface string
-	Feature []string
-}
-// ParsedSen ParsedSentence struct (plain: plain, morps: Morphemes)
-type ParsedSen struct {
-	Plain string
-	Morps []Morp
-}
 
-// Parse Parse input string (str: input string)
-func (p *ParsedSen) Parse(str string) int {
+// ParseMeCab Parse input string using MeCab (str: input string)
+func (p *Sentence) ParseMeCab(str string) int {
 	p.Plain = str
+	p.MorphemeMap = make(map[string]*Morpheme)
 	tagger, err := mecab.New(map[string]string{"dicdir": "/usr/lib/mecab/dic/mecab-ipadic-neologd"})
 	defer tagger.Destroy()
 	if (err != nil) {
@@ -34,44 +25,29 @@ func (p *ParsedSen) Parse(str string) int {
 	for _, ele := range strings.Split(result, "\n") {
 		word:= strings.Split(ele, "\t")
 		if (len(word) > 1) {
-			p.Morps = append(p.Morps, Morp{Surface: word[0], Feature: strings.Split(word[1], ",")})
+			p.Morphemes = append(p.Morphemes, Morpheme{Surface: word[0], Feature: strings.Split(word[1], ",")})
+			p.MorphemeMap[word[0]] = &p.Morphemes[len(p.Morphemes) - 1]
 		}
 	}
 	return 0
 }
 
-func MorpIsIndep(wclass string) bool {
+func MorphemeIsIndep(wclass string) bool {
 	if (wclass == "助詞" || wclass == "助動詞" || wclass == "記号") {
 		return false
-	} else {
-		return true
 	}
+	return true
 }
-func (m *Morp) IsIndep() bool {
-	return MorpIsIndep(m.Feature[0])
+func (m *Morpheme) IsIndep() bool {
+	return MorphemeIsIndep(m.Feature[0])
 }
 
-func (p *ParsedSen) CountIndepMorps() int {
+func (p *Sentence) CountIndepMorphemes() int {
 	mcount := 0
-	for _, ele := range p.Morps {
+	for _, ele := range p.Morphemes {
 		if ele.IsIndep() {
 			mcount++
 		}
 	}
 	return mcount
-}
-func (p *ParsedSen) CalcTF(m Morp) float32 {
-	mcount := 0
-	for _, ele := range p.Morps {
-		if m.Surface == ele.Surface {
-			mcount++
-		}
-	}
-	return float32(mcount) / float32(p.CountIndepMorps())
-}
-
-func SplitText(text string) []string {
-	return strings.FieldsFunc(text, func(r rune) bool {
-		return (r == '。' || r == '．' || r == '\n' || r == '！' || r == '？')
-	})
 }
